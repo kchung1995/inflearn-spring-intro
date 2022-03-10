2ee64357d65e4530759ea2a2ebfbd155be2cb130
KuyHo <kchung1995@gmail.com>
2022-03-10 16:22:21 +0900

    스프링 통합 테스트

---
+++ src/test/java/com/tistory/katfun/intro/service/MemberServiceIntegrationTest.java
@@ -0,0 +1,56 @@
+package com.tistory.katfun.intro.service;
+
+import com.tistory.katfun.intro.domain.Member;
+import com.tistory.katfun.intro.repository.MemberRepository;
+import com.tistory.katfun.intro.repository.MemoryMemberRepository;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+@Transactional
+class MemberServiceIntegrationTest {
+
+    @Autowired MemberService memberService;
+    @Autowired MemberRepository memberRepository;
+
+
+    @Test
+    void 회원가입() {
+        //given
+        Member member = new Member();
+        member.setName("spring");
+
+        //when
+        Long saveId = memberService.join(member);
+
+        //then
+        Member findMember = memberService.findOne(saveId).get();
+        assertEquals(member.getName(),findMember.getName());
+    }
+
+    @Test
+    public void 중복_회원_예외() {
+        //given
+        Member member1 = new Member();
+        member1.setName("spring");
+
+        Member member2 = new Member();
+        member2.setName("spring");
+
+        //when
+        memberService.join(member1);
+        IllegalStateException e = assertThrows(IllegalStateException.class, () -> memberService.join(member2));
+
+        //then
+        assertEquals(e.getMessage(), "이미 존재하는 회원입니다.");
+
+    }
+
+}
